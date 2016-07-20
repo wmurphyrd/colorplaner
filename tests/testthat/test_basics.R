1b5a58470bb5336245d5d3a103d8a5d492dd967d
Will Murphy <wmurphy@eatright.org>
2016-07-20 15:32:24 -0500

    removed Stat implementation and implemented as a Scale instead

---
+++ tests/testthat/test_basics.R
@@ -0,0 +1,7 @@
+test_that("Basic plotting occurs without error", {
+  expect_silent(
+    ggplot(mtcars, aes(x = wt, y = mpg, color = disp, colour2 = hp)) +
+    geom_point() +
+    scale_color_colorplane()
+  )
+})
