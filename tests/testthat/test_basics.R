1fd09a17b2f871e1d411cf98181d8fa0c7a5648c
Will Murphy <wmurphy@eatright.org>
2016-07-22 14:12:20 -0500

    added scale_fill_colorplane function to setup fill-based scales

--- tests/testthat/test_basics.R
+++ tests/testthat/test_basics.R
@@ -1,7 +1,17 @@
 test_that("Basic plotting occurs without error", {
-  expect_silent(
+  expect_silent({
     ggplot(mtcars, aes(x = wt, y = mpg, color = disp, colour2 = hp)) +
     geom_point() +
     scale_color_colorplane(guide = guide_colorplane())
-  )
+
+    ggplot(mtcars, aes(x = wt, y = mpg, fill = disp, fill2 = hp)) +
+      geom_point(shape = 23, size = 5) +
+      scale_fill_colorplane(guide = guide_colorplane())
+
+    ggplot(mtcars, aes(x = wt, y = mpg, fill = disp, fill2 = hp,
+                       color = gear, color2 = carb)) +
+      geom_point(shape = 23, size = 5) +
+      scale_fill_colorplane(guide = guide_colorplane()) +
+      scale_color_colorplane(guide = guide_colorplane())
+  })
 })
