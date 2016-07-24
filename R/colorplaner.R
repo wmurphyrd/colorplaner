8d15afc3938a7d47e1aa299a94fd6880623e92ce
Will Murphy <william@datatitian.com>
2016-07-23 21:28:58 -0500

    added GPL copyright notice

--- R/colorplaner.R
+++ R/colorplaner.R
@@ -1,12 +1,13 @@
 #' colorplaner: ggplot2 Extenstion to Visualize Two Variables Per Color
 #' Aesthetic through Color Space Projection
 #'
-#' Add additional dimensionality to
-#' visualizations by using the color and/or fill aesthetics to convey the values
-#' of two continous variables each. By projecting variable values onto YUV
-#' colorspace, a scale is created that allows viewers to intuitively determine
-#' the values of both variables from the single displayed color. Includes two
-#' new scales and a new guide for ggplot2.
+#' Add additional dimensionality to visualizations by using the color and/or
+#' fill aesthetics to convey the values of two continous variables each. By
+#' projecting variable values onto YUV colorspace, a scale is created that
+#' allows viewers to intuitively determine the values of both variables from the
+#' single displayed color. Includes two new scales and a new guide for ggplot2.
+#'
+#'
 #'
 #' @docType package
 #' @name colorplaner
