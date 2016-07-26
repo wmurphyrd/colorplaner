089093916f07d32b5061bbdf23f95b2de2629d0b
Will Murphy <william@datatitian.com>
2016-07-25 21:04:26 -0500

    import 'is' from methods for CMD check note, improve makeReadme.R, minor text edits in vignette

--- R/colorplaner.R
+++ R/colorplaner.R
@@ -14,4 +14,5 @@
 #' @import ggplot2
 #' @importFrom scales rescale
 #' @importFrom scales censor
+#' @importFrom methods is
 NULL
--- makeReadme.R
+++ makeReadme.R
@@ -1,1 +1,21 @@
-knitr::knit("vignettes/colorplaner.Rmd", "readme.md")
+
+readme <- readLines("vignettes/colorplaner.Rmd")
+loc <- grep("^---$", readme)
+
+ins <- c("[![BuildStatus](https://travis-ci.org/wmurphyrd/colorplaner.svg?branch=master)](https://travis-ci.org/wmurphyrd/colorplaner)",
+         "",
+         "# colorplaner",
+         "",
+         "To install:",
+         "",
+         "```{r, eval=FALSE}",
+         "devtools::install_github(\"wmurphyrd/colorplaner\")",
+         "```",
+         "",
+         "```{r include = F}",
+         "knitr::opts_chunk$set(fig.width = 5, fig.height = 5)",
+         "```",
+         "")
+
+knitr::knit(text = c(ins, readme[seq(loc[2] + 1, length(readme))]),
+            output = "readme.md", quiet = TRUE)
