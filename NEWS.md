83fbaa6a552b0342fa216e0ba2a4c002ae568afb
Will Murphy <wmurphy@eatright.org>
2016-07-31 11:27:43 -0500

    advance to v 0.0.0.9001, add news.md, update readme to include news

---
+++ NEWS.md
@@ -0,0 +1,10 @@
+# Known Issues and Incomplete Features
+* Cannot alter title and axis label positions in guide_colorplane (#2)
+* Large list of dependencies and namespace attachments inherited from colorscience package (#4)
+* Colors render incorrectly when discrete variables assigned to color/colour/fill (#1) 
+
+# Version 0.0.0.9001
+* Removed all dependencies on unexported objects from ggplot2
+
+# Version 0.0.0.9000
+* Adds `scale_color_colorplane`, `scale_fill_colorplane`, and `guide_colorplane` extensions to ggplot2
--- makeReadme.R
+++ makeReadme.R
@@ -2,22 +2,30 @@
 readme <- readLines("vignettes/colorplaner.Rmd")
 loc <- grep("^---$", readme)
 
+news_loc <- grep("^## Usage$", readme)[1] - 1
+
 ins <- c("[![BuildStatus](https://travis-ci.org/wmurphyrd/colorplaner.svg?branch=master)](https://travis-ci.org/wmurphyrd/colorplaner)",
-         "",
-         "# colorplaner",
-         "",
-         "To install:",
-         "",
-         "```{r, eval=FALSE}",
-         "devtools::install_github(\"wmurphyrd/colorplaner\")",
-         "```",
          "",
          "```{r include = F}",
          "knitr::opts_chunk$set(fig.width = 5, fig.height = 5)",
          "```",
          "",
-         "Branch `internalize`: removes dependencies on internal objects in `ggplot2` and passes R CMD check without notes",
+         "# colorplaner",
          "")
+news <- readLines("NEWS.md")
+news <- gsub("^#", "####", news)
+news_ins <- c("",
+              "## Installation",
+              "",
+              "To install:",
+              "",
+              "```{r, eval=FALSE}",
+              "devtools::install_github(\"wmurphyrd/colorplaner\")",
+              "```",
+              "",
+              news,
+              "")
 
-knitr::knit(text = c(ins, readme[seq(loc[2] + 1, length(readme))]),
+knitr::knit(text = c(ins, readme[seq(loc[2] + 1, news_loc)],
+                     news_ins, readme[seq(news_loc + 1, length(readme))]),
             output = "readme.md", quiet = TRUE)
