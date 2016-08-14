6b156d3bef617a0ab36380e191b001a2443ef3bf
Will Murphy <wmurphy@eatright.org>
2016-08-14 16:30:24 -0500

    ensure tests complete full ggplot construction process

--- tests/testthat/test_basics.R
+++ tests/testthat/test_basics.R
@@ -1,42 +1,42 @@
 test_that("Basic plotting occurs without error", {
   expect_silent({
-    ggplot(mtcars, aes(x = wt, y = mpg, color = disp, colour2 = hp)) +
+    print(ggplot(mtcars, aes(x = wt, y = mpg, color = disp, colour2 = hp)) +
     geom_point() +
-    scale_color_colorplane()
+    scale_color_colorplane())
 
-    ggplot(mtcars, aes(x = wt, y = mpg, fill = disp, fill2 = hp)) +
+    print(ggplot(mtcars, aes(x = wt, y = mpg, fill = disp, fill2 = hp)) +
       geom_point(shape = 23, size = 5) +
-      scale_fill_colorplane()
+      scale_fill_colorplane())
 
-    ggplot(mtcars, aes(x = wt, y = mpg, fill = disp, fill2 = hp,
+    print(ggplot(mtcars, aes(x = wt, y = mpg, fill = disp, fill2 = hp,
                        color = gear, color2 = carb)) +
       geom_point(shape = 23, size = 5) +
       scale_fill_colorplane() +
-      scale_color_colorplane()
+      scale_color_colorplane())
   })
 })
 
 test_that("Transformed colorplane scale plots without error", {
   expect_silent({
-    ggplot(mtcars, aes(x = wt, y = mpg, color = disp, colour2 = hp)) +
+    print(ggplot(mtcars, aes(x = wt, y = mpg, color = disp, colour2 = hp)) +
       geom_point() +
-      scale_color_colorplane(trans = "log")
-    ggplot(mtcars, aes(x = wt, y = mpg, fill = disp, fill2 = hp)) +
+      scale_color_colorplane(trans = "log"))
+    print(ggplot(mtcars, aes(x = wt, y = mpg, fill = disp, fill2 = hp)) +
       geom_point(shape = 23, size = 5) +
-      scale_fill_colorplane(trans = "log")
+      scale_fill_colorplane(trans = "log"))
     })
 })
 
 test_that("OOB and NA data handled without error", {
   expect_silent({
-    ggplot(mtcars, aes(x = wt, y = mpg, color = qsec, colour2 = hp)) +
+    print(ggplot(mtcars, aes(x = wt, y = mpg, color = qsec, colour2 = hp)) +
       geom_point(size = 4) +
       scale_color_colorplane(limits = c(NA, 18.9), limits_y = c(60, 300),
-                             na.color = "yellow")
-    ggplot(mtcars, aes(x = wt, y = mpg, fill = qsec, fill2 = hp)) +
+                             na.color = "yellow"))
+    print(ggplot(mtcars, aes(x = wt, y = mpg, fill = qsec, fill2 = hp)) +
       geom_point(shape = 23, size = 5) +
       scale_fill_colorplane(limits = c(NA, 18.9), limits_y = c(60, 300),
-                            na.color = "yellow")
+                            na.color = "yellow"))
     })
 
 })
