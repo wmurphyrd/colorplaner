7838297f36077ac8b326faa15fbd2c3c5a0e7d2b
Will Murphy <william@datatitian.com>
2016-09-05 13:36:47 -0500

    add titles to plots in tests to make resulting pdf easier to read

--- tests/testthat/test_basics.R
+++ tests/testthat/test_basics.R
@@ -2,17 +2,20 @@ test_that("Basic plotting occurs without error", {
   expect_silent({
     print(ggplot(mtcars, aes(x = wt, y = mpg, color = disp, colour2 = hp)) +
     geom_point() +
-    scale_color_colorplane())
+    scale_color_colorplane() +
+    ggtitle("Colored points"))
 
     print(ggplot(mtcars, aes(x = wt, y = mpg, fill = disp, fill2 = hp)) +
       geom_point(shape = 23, size = 5) +
-      scale_fill_colorplane())
+      scale_fill_colorplane() +
+      ggtitle("Filled diamond points"))
 
     print(ggplot(mtcars, aes(x = wt, y = mpg, fill = disp, fill2 = hp,
                        color = gear, color2 = carb)) +
       geom_point(shape = 23, size = 5) +
       scale_fill_colorplane() +
-      scale_color_colorplane())
+      scale_color_colorplane() +
+      ggtitle("Filled and colored diamonds"))
   })
 })
 
@@ -20,10 +23,12 @@ test_that("Transformed colorplane scale plots without error", {
   expect_silent({
     print(ggplot(mtcars, aes(x = wt, y = mpg, color = disp, colour2 = hp)) +
       geom_point() +
-      scale_color_colorplane(trans = "log"))
+      scale_color_colorplane(trans = "log") +
+      ggtitle("Log trans colored points"))
     print(ggplot(mtcars, aes(x = wt, y = mpg, fill = disp, fill2 = hp)) +
       geom_point(shape = 23, size = 5) +
-      scale_fill_colorplane(trans = "log"))
+      scale_fill_colorplane(trans = "log") +
+      ggtitle("Log trans filled diamonds"))
     })
 })
 
@@ -32,11 +37,13 @@ test_that("OOB and NA data handled without error", {
     print(ggplot(mtcars, aes(x = wt, y = mpg, color = qsec, colour2 = hp)) +
       geom_point(size = 4) +
       scale_color_colorplane(limits = c(NA, 18.9), limits_y = c(60, 300),
-                             na.color = "yellow"))
+                             na.color = "yellow") +
+      ggtitle("Yellow out-of-bounds points"))
     print(ggplot(mtcars, aes(x = wt, y = mpg, fill = qsec, fill2 = hp)) +
       geom_point(shape = 23, size = 5) +
       scale_fill_colorplane(limits = c(NA, 18.9), limits_y = c(60, 300),
-                            na.color = "yellow"))
+                            na.color = "yellow") +
+      ggtitle("Yellow out-of-bounds diamonds"))
     })
 
 })
@@ -51,7 +58,8 @@ test_that("Arguments can be passed to projection functions", {
     print(ggplot(mtcars, aes(x = wt, y = mpg, color = qsec, colour2 = hp)) +
             geom_point(size = 4) +
             scale_color_colorplane(limits = c(NA, 18.9), limits_y = c(60, 300),
-                                   na.color = "red", Y = .9))
+                                   na.color = "red", Y = .9) +
+            ggtitle("Bright colored points"))
   })
 })
 
--- tests/testthat/test_projections.R
+++ tests/testthat/test_projections.R
@@ -2,11 +2,13 @@ test_that("Alt projection red_blue can be used", {
   expect_silent({
     print(ggplot(mtcars, aes(x = wt, y = mpg, color = disp, colour2 = hp)) +
             geom_point() +
-            scale_color_colorplane(color_projection = "red_blue"))
+            scale_color_colorplane(color_projection = "red_blue") +
+            ggtitle("Red-blue color projection"))
 
     print(ggplot(mtcars, aes(x = wt, y = mpg, color = disp, colour2 = hp)) +
             geom_point() +
-            scale_color_colorplane(color_projection = red_blue_projection))
+            scale_color_colorplane(color_projection = red_blue_projection) +
+            ggtitle("Red-blue color projection"))
   })
 })
 
@@ -17,6 +19,7 @@ test_that("Interpolation projection can be used", {
             scale_color_colorplane(color_projection = "interpolate",
                                    zero_color = "darkorange2",
                                    horizontal_color = "mediumspringgreen",
-                                   vertical_color = "#CD00CD"))
+                                   vertical_color = "#CD00CD") +
+            ggtitle("Orange-green-magenta projection"))
   })
 })
